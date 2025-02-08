b35bd424cff3fc3b7eb946beada37e7f5f3c1d93
Shadoworbs <saieedadam101@gmail.com>
2025-02-08 15:38:18 +0000

    Refactor downloaders to improve folder management and update file naming conventions

--- helper/downloaders.py
+++ helper/downloaders.py
@@ -3,8 +3,12 @@ from datetime import datetime
 from yt_dlp import YoutubeDL
 from helper.functions import getsize, convert_seconds, log
 import os
+
+
 cwd = os.getcwd()
 now = datetime.now
+audio_folder, video_folder = "audio", "videos"
+timestamps = now().strftime('%Y%m%d%H%M%S')
 
 
 
@@ -12,31 +16,31 @@ now = datetime.now
 ###########    Video Downloader   #############
 ###############################################
 
-# function to download the video only
+# function to download video only
 def download_video(url, video_height, fps=30) -> tuple:
-    if os.path.exists('videos'):
+    global cwd, now, timestamps, video_folder
+    if os.path.exists(video_folder):
         try:
-            os.chdir('videos')
+            os.chdir(video_folder)
         except Exception as e:
-            print(f"Can't change directory to /videos \n{e}")
+            print(f"Can't change directory to {video_folder} \n{e}")
             pass
     else:
         try:
-            os.mkdir('videos')
-            os.chdir('videos')
+            os.mkdir(video_folder)
+            os.chdir(video_folder)
         except Exception as e:
-            print(f"Can't create directory /videos\n{e}")
+            print(f"Can't create directory {video_folder}\n{e}")
             pass
 
     # set up the video download options
     opts = {"trim_file_name": 200,
-            'outtmpl': '%(title)s_%(id)s_%(resolution)s.%(ext)s',
+            'outtmpl': '%(title)s_{timestamps}.%(ext)s'.format(timestamps=timestamps),
             "encoding": "utf-8",
             "format": f"((bv*[ext=mp4])[height<={video_height}]/(wv*[ext=mp4]/wv*)) + (ba[ext=mp3]/ba) / (b[fps<={fps}]/b)[height<={video_height}]/(w[fps<={fps}]/w)",
             "playlist": True,
             "cookiefile": "cookies_from_browser firefox",
-            # "outtmpl": "%(title)s.%(ext)s",
-            # "merge_output_format": "mp4"
+            "merge_output_format": "mp4"
             } # using a merge output format that will result in a low size video
     with YoutubeDL(opts) as ydl:
         info_dict = ydl.extract_info(url, download=True)
@@ -45,20 +49,18 @@ def download_video(url, video_height, fps=30) -> tuple:
         # dur: str = info_dict.get('duration_string', str)
         raw_resolution: str = info_dict.get('resolution', str)
         # size: str = info_dict.get('filesize')
-        video_id: str = info_dict.get('id', str)
+        # video_id: str = info_dict.get('id', str)
         video_duration = info_dict.get('duration', str)
         video_resolution = raw_resolution.split('x')[-1] or video_height
 
-    # assign the return value convert_seconds to a variable
+    # assign the return value ofconvert_seconds function to a variable
     readable_duration = convert_seconds(video_duration)
     # assign the return value of getsize function to a variable
-    get_size = getsize(video_id, raw_resolution, ext=extension, mode='video')
-    # rename the video to it's original name
-    # renamer(video_title, extension, video_id, video_resolution, cwd, raw_res=raw_resolution, sec_name=secondary_name)
-
+    get_size = getsize(timestamps)
+  
     # prepare a info (dict) for logging
-    log_inf = {"Video Name": f'{video_title}_{video_id}_{raw_resolution}.{extension}',
-                "Location": cwd,
+    log_inf = {"Video Name": f'{video_title}_{timestamps}.{extension}',
+                "Location": f"{cwd}\\{video_folder}",
                 "Duration": readable_duration,
                 "Resolution": f'{video_resolution}p',
                 "Size": get_size,
@@ -71,8 +73,8 @@ def download_video(url, video_height, fps=30) -> tuple:
     # print a success message after download completes
     print(f"""
 Download complete!
-Video name: {video_title}_{video_id}_{raw_resolution}.{extension}
-Video location : {cwd}
+Video name: {video_title}_{timestamps}.{extension}
+Video location : {cwd}\\{video_folder}
 Video duration: {readable_duration}
 Video resolution: {video_resolution}p
 Video Size: {get_size}""")
@@ -86,39 +88,40 @@ Video Size: {get_size}""")
 
 # function to download the audio only
 def download_audio(url):
+    global cwd, now, timestamps, audio_folder
     # Create folders for storing downloaded media
-    if os.path.exists('audio'):
+    if os.path.exists(audio_folder):
         try:
-            os.chdir('audio')
+            os.chdir(audio_folder)
         except:
-            print("Couldn't change directory to 'audio'")
+            print(f"Couldn't change directory to {audio_folder}")
             pass
     else:
         try:
-            os.mkdir('audio')
-            os.chdir('audio')
+            os.mkdir(audio_folder)
+            os.chdir(audio_folder)
         except:
-            print("Couldn't create directory 'audio'")
+            print(f"Couldn't create directory {audio_folder}")
             pass
     # start the dowload process
     with YoutubeDL({'extract_audio': True, 
                             "format": "bestaudio[ext=m4a]/b", 
-                            'outtmpl': '%(title)s_%(id)s.%(ext)s',
+                            'outtmpl': '%(title)s_{timestamps}.%(ext)s'.format(timestamps=timestamps),
                             "cookiefile": "cookies_from_browser firefox"}) as audio:
         info_dict = audio.extract_info(url, download=True) # download the audio
         audio_title = info_dict['title'] # extract the title from the url info.json
         extension = info_dict['ext'] # extract the extension from the url info.json
         audio_duration = info_dict.get('duration', str)  # extract the duration fron the url info.json
-        audio_id = info_dict.get('id', str) # get the audio id
+        # audio_id = info_dict.get('id', str) # get the audio id
 
         # assign the return value of convert seconds functioni to a varible
         readable_duration = convert_seconds(audio_duration)
         # assign the return value of getsize to a variable
-        get_size = getsize(id=audio_id, ext=extension, mode='audio')
+        get_size = getsize(timestamps)
 
         # set up the audio information dictionary for logging
-        audio_infos = {"Title": f'{audio_title}_{audio_id}.{extension}',
-                        "Location": cwd,
+        audio_infos = {"Title": f'{audio_title}_{timestamps}.{extension}',
+                        "Location": f"{cwd}\\{audio_folder}",
                         "Duration": readable_duration,
                         "Size": get_size,
                         "Timestamp": f'{now().strftime("%A, %B %d %Y | %I:%M %p")}'}
@@ -129,8 +132,8 @@ def download_audio(url):
         # print success message after downloading is complete
         print(f"""
 Download complete!
-Title: {audio_title}_{audio_id}.{extension}
-location: {cwd}
+Title: {audio_title}_{timestamps}.{extension}
+location: {cwd}\\{audio_folder}
 Duration: {readable_duration}
 Size: {get_size}
 """)
